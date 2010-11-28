3d1a8620f66597c866777bffa13492ce300ece90
robwilliams <robwilliams@localhost>
2010-11-27 23:10:03 -0700

    got Nose initially running and builds sdist

---
+++ README.txt
@@ -0,0 +1,6 @@
+=====
+Build
+=====
+
+Build provides a practical illustration of a Python build environment.
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Build',
+    version='0.1',
+    author='Rob Williams',
+    author_email='Robert.David.Williams@gmail.com',
+    url='http://bitbucket.org/robwilliams/build/',
+    license='LICENSE.txt',
+    description='Illustrate a Python build environment.',
+    long_description=open('README.txt').read(),
+)
+
