cf1a821f98f20be470a3ba051da166fde8bb9a97
robwilliams <robwilliams@localhost>
2011-06-08 21:19:25 -0600

    added src/test/euler002_test.py changed src/test/euler001_test.py to record solution date

---
+++ src/test/euler002_test.py
@@ -0,0 +1,13 @@
+''' Project Euler Problem 2
+REF:  http://projecteuler.net/index.php?section=problems&id=2
+Created:  
+Solved:
+
+
+'''
+
+# Form the solution
+
+# Test the solution
+
+# Report the solution
