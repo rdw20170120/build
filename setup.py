79df71632da45503a9c92759fbf2cdb61abf4750
robwilliams <robwilliams@localhost>
2011-07-10 23:45:08 -0600

    Refactor project structure, improve sloccount results

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Build',
-    version='0.2',
+    version='0.3',
     author='Rob Williams',
     author_email='Robert.David.Williams@gmail.com',
     url='http://bitbucket.org/robwilliams/build/',
-    license='LICENSE.txt',
+    license='doc/LICENSE.txt',
     description='Illustrate a Python build environment.',
-    long_description=open('README.txt').read(),
+    long_description=open('doc/README.txt').read(),
 )
