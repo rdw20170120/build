548c17cb4e6096ae0c16909d8cc7e80e39224238
robwilliams <robwilliams@localhost>
2011-10-31 14:52:29 -0600

    Refine establishment of BO_Home

--- activate_Python_project.bash
+++ activate_Python_project.bash
@@ -2,9 +2,9 @@
 
 echo 'INFO:  Activating the Python project...'
 
-source $HomeBuildTools/bin/Python/helper/configure_TMPDIR
-source $HomeBuildTools/bin/Python/helper/configure_pip
-source $HomeBuildTools/bin/Python/helper/configure_virtualenv
-source $HomeBuildTools/bin/Python/helper/configure_PATH
-source $HomeBuildTools/bin/Python/helper/configure_output
+source $BO_Home/bin/Python/helper/configure_TMPDIR
+source $BO_Home/bin/Python/helper/configure_pip
+source $BO_Home/bin/Python/helper/configure_virtualenv
+source $BO_Home/bin/Python/helper/configure_PATH
+source $BO_Home/bin/Python/helper/configure_output
 
