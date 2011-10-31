6eaf02dd2e2d1df4bd0ce1ac9f62d7ca0699a12b
robwilliams <robwilliams@localhost>
2011-10-31 12:16:17 -0600

    Tweaking

---
+++ activate_Python_project.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'INFO:  Activating the Python project...'
+
+source $HomeBuildTools/bin/Python/helper/configure_TMPDIR
+source $HomeBuildTools/bin/Python/helper/configure_pip
+source $HomeBuildTools/bin/Python/helper/configure_virtualenv
+source $HomeBuildTools/bin/Python/helper/configure_PATH
+source $HomeBuildTools/bin/Python/helper/configure_output
+
--- configure_Python_project.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'INFO:  Configure project (general)'
-
-source $HomeBuildTools/bin_shared/helper/configure_TMPDIR
-source $HomeBuildTools/bin_shared/helper/configure_pip
-source $HomeBuildTools/bin_shared/helper/configure_virtualenv
-source $HomeBuildTools/bin_shared/helper/configure_PATH
-source $HomeBuildTools/bin_shared/helper/configure_output
