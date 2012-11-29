7ec72b274b2e52f5ff7eb656bc43232f2e229e55
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2012-11-29 16:49:10 -0700

    Integrate Gradle

--- README.rst
+++ README.rst
@@ -20,6 +20,23 @@ Windows 7 and Windows 2008 R2.  Python means 2.6+, Java means 1.6+, while
 CoffeeScript and .NET will be recent versions but are not yet precisely
 defined.
 
+I don't like the implementation at the moment, which is constrained by the
+design decision to 'source' the activate.bash script.  Doing so means that I
+cannot call 'exit' from any of the scripts since it will terminate the user's
+shell (close the terminal).  Instead, I will soon redesign so that only the
+cfg/project-env.bash script is 'source'd.  That will allow all the scripts to
+invoke 'exit' safely, which will dramatically simplify the code.
+
+On the other hand, I also intend to require Gradle as the primary build
+scripting tool soon.  That will also dramatically simplify the code by moving
+it into the realm of Gradle's ample functionality.
+
+Beyond that, I am thinking that I want to support significant functionality
+without Gradle for languages that do not really benefit as much from it,
+meaning Python and CoffeeScript.  Since Gradle does not (yet) provide
+dependency management for those languages, there is less to be gained by
+involving Gradle in the first place.
+
 NOTE: This file is designed for the hosting of this project at BitBucket_, so
 it is formatted using reStructuredText_ syntax.
 
---
+++ activate_Gradle_project.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo 'INFO: Activating the Gradle project...'
+
+# NOTE: This script should be sourced into the shell environment
+# NOTE: This script, and EVERY script that it calls, must NOT invoke 'exit'!
+#       The user calling this script must be allowed to preserve their shell
+#       and every effort must be made to inform the user of problems while
+#       continuing execution where possible.  Exiting the shell robs the user
+#       of useful feedback and interrupts their work, which is unacceptable.
+
+[[   -z "$BO_Home"    ]] && echo 'FATAL: Missing $BO_Home'             && return 1
+[[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'          && return 1
+[[ ! -d "$BO_Home"    ]] && echo "FATAL: Missing directory '$BO_Home'" && return 1
+
+# Configure the Linux environment
+_Dir=$BO_Home/bin/Linux/helper
+[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
+source ${_Dir}/declare.bash
+[[ $? -ne 0 ]] && echo 'FATAL: Aborting' && return 1
+
+logInfo "BriteOnyx scripting support loaded!"
+# NOTE: Now we have our special BriteOnyx scripting functionality loaded, so we
+#       can shift to using that rather than directly invoking BASH primitives.
+
+# Configure the Gradle environment
+logDebug 'Configuring temporary directory $TMPDIR...'
+requireVariable TMPDIR  || return 1
+createDirectory $TMPDIR || return 1
+logDebug 'Configuring output directory...'
+requireVariable BO_Project      || return 1
+createDirectory $BO_Project/out || return 1
+logDebug 'Configuring system execution $PATH...'
+export BO_PathSystem=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+requireVariable GRADLE_HOME   || return 1
+requireVariable JAVA_HOME     || return 1
+requireVariable BO_Home       || return 1
+requireVariable BO_PathSystem || return 1
+requireVariable BO_Project    || return 1
+_PathBuild=$BO_Home/bin/Linux
+_PathGradle=$GRADLE_HOME/bin
+_PathJava=$JAVA_HOME/bin
+_PathProject=$BO_Project/bin
+export PATH=${_PathProject}:${_PathBuild}:${_PathGradle}:${_PathJava}:${BO_PathSystem}
+
+: <<'DisabledContent'
+DisabledContent
+
+# Return, but do NOT exit, with a success code
+logInfo "Gradle project '$BO_Project' activated."
+return 0
+
