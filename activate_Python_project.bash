51b1a358f1f3eb805b5da036ab8ae9bbcd359da9
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2013-12-30 16:55:02 -0700

    Begin redesign of Python activation script to avoid 'exit' problems on Linux

--- activate_Python_project.bash
+++ activate_Python_project.bash
@@ -1,40 +1,58 @@
 #!/bin/bash
-echo 'INFO: Activating the Python project...'
+# NOTE: This script must NOT be 'source'd into a user shell since it might 'exit'
+# NOTE: This script MUST be 'source'd from other scripts
+echo 'INFO: BriteOnyx is activating Python for this project...'
 
-# NOTE: This script should be sourced into the shell environment
-# NOTE: This script, and EVERY script that it calls, must NOT invoke 'exit'!
-#       The user calling this script must be allowed to preserve their shell
-#       and every effort must be made to inform the user of problems while
-#       continuing execution where possible.  Exiting the shell robs the user
-#       of useful feedback and interrupts their work, which is unacceptable.
-
-[[   -z "$BO_Home"    ]] && echo 'FATAL: Missing $BO_Home'             && return 1
-[[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'          && return 1
-[[ ! -d "$BO_Home"    ]] && echo "FATAL: Missing directory '$BO_Home'" && return 1
+[[   -z "$BO_Home"    ]] && echo 'FATAL: Missing $BO_Home'                && exit 1
+[[ ! -d "$BO_Home"    ]] && echo "FATAL: Missing directory '$BO_Home'"    && exit 1
+[[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'             && exit 1
+[[ ! -d "$BO_Project" ]] && echo "FATAL: Missing directory '$BO_Project'" && exit 1
 
 # Configure the Linux environment
 _Dir=$BO_Home/bin/Linux/helper
-[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
+[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && exit 1
 source ${_Dir}/declare.bash
-[[ $? -ne 0 ]] && echo 'FATAL: Aborting' && return 1
+[[ $? -ne 0 ]] && echo 'FATAL: Aborting' && exit 1
 
 logInfo "BriteOnyx scripting support loaded!"
 # NOTE: Now we have our special BriteOnyx scripting functionality loaded, so we
 #       can shift to using that rather than directly invoking BASH primitives.
 
-# Configure the Python environment
-_Dir=$BO_Home/bin/Python/helper
-requireDirectory ${_Dir}            || return 1
-source ${_Dir}/configure_TMPDIR     || return 1
-source ${_Dir}/configure_output     || return 1
-source ${_Dir}/configure_pip        || return 1
-source ${_Dir}/configure_virtualenv || return 1
-source ${_Dir}/configure_PATH       || return 1
+# Configure environment for Python on Linux
+requireVariable BO_Home
+requireVariable BO_Project
 
-: <<'DisabledContent'
-DisabledContent
+_Dir=$BO_Home/bin/Python/helper
+requireDirectory ${_Dir}
+
+source ${_Dir}/configure_TMPDIR
+abortOnFail "$?"
+source ${_Dir}/configure_output
+abortOnFail "$?"
+source ${_Dir}/configure_pip
+abortOnFail "$?"
+source ${_Dir}/configure_virtualenv
+abortOnFail "$?"
+source ${_Dir}/configure_PATH
+abortOnFail "$?"
 
 # Return, but do NOT exit, with a success code
-logInfo "Python project '$BO_Project' activated."
+logInfo "BriteOnyx has activated Python for project '$BO_Project'."
 return 0
+
+: <<'DisabledContent'
+# Must be first in PATH
+_PathProject=$BO_Project/bin/Linux
+PATH=${_PathProject}
+
+_PathBuild=$BO_Home/bin/Linux
+PATH=$PATH:${_PathBuild}
+
+# TODO: Move to user-specific configuration file?
+export BO_PathSystem=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+requireVariable BO_PathSystem
+PATH=$PATH:${BO_PathSystem}
+
+export PATH
+DisabledContent
 
