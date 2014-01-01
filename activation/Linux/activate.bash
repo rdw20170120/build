e7397da46f58effbd9b558c1de61cc07dc383d38
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 15:10:58 -0700

    Revisit design of 'activation' scripts

--- activation/Linux/activate.bash
+++ activation/Linux/activate.bash
@@ -1,34 +1,35 @@
 #!/bin/bash
 echo "TRACE: Executing '$BASH_SOURCE'"
 
-[[   -z "$BO_Home"    ]] && echo 'FATAL: Missing $BO_Home'                && return 1
-[[ ! -d "$BO_Home"    ]] && echo "FATAL: Missing directory '$BO_Home'"    && return 1
-[[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'             && return 1
-[[ ! -d "$BO_Project" ]] && echo "FATAL: Missing directory '$BO_Project'" && return 1
+[[   -z "$BO_Home"       ]] && echo 'FATAL: Missing $BO_Home'                && return 1
+[[ ! -d "$BO_Home"       ]] && echo "FATAL: Missing directory '$BO_Home'"    && return 1
+[[   -z "$BO_Project"    ]] && echo 'FATAL: Missing $BO_Project'             && return 1
+[[ ! -d "$BO_Project"    ]] && echo "FATAL: Missing directory '$BO_Project'" && return 1
+[[   -z "$BO_PathSystem" ]] && echo 'FATAL: Missing $BO_PathSystem'          && return 1
 
 # Configure the Linux environment
+
 _Dir=$BO_Home/activation/Linux
 [[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
+
 _Script=${_Dir}/declare.bash
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
+
 source ${_Script}
+
 _ExitCode=$?
 [[ ${_ExitCode} -ne 0 ]] && \
   echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
   return ${_ExitCode}
 
-# Must be first in PATH
-_PathProject=$BO_Project/bin/Linux
-PATH=${_PathProject}
+BO_PathProject=$BO_Project/bin/Linux
+BO_PathLinux=$BO_Home/invocation/Linux
 
-_PathBuild=$BO_Home/invocation/Linux
-PATH=$PATH:${_PathBuild}
-
-[[ -z "$BO_PathSystem" ]] && echo 'FATAL: Missing $BO_PathSystem' && return 1
+PATH=${BO_PathProject}
+PATH=$PATH:${BO_PathLinux}
 PATH=$PATH:${BO_PathSystem}
-
 export PATH
 
 # Configure TMPDIR
--- activation/Python/activate.bash
+++ activation/Python/activate.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "TRACE: Executing '$BASH_SOURCE'"
 
-[[   -z "$BO_Home"    ]] && echo 'FATAL: Missing $BO_Home'                && return 1
-[[ ! -d "$BO_Home"    ]] && echo "FATAL: Missing directory '$BO_Home'"    && return 1
-[[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'             && return 1
-[[ ! -d "$BO_Project" ]] && echo "FATAL: Missing directory '$BO_Project'" && return 1
+[[   -z "$BO_Home"       ]] && echo 'FATAL: Missing $BO_Home'                && return 1
+[[ ! -d "$BO_Home"       ]] && echo "FATAL: Missing directory '$BO_Home'"    && return 1
+[[   -z "$BO_Project"    ]] && echo 'FATAL: Missing $BO_Project'             && return 1
+[[ ! -d "$BO_Project"    ]] && echo "FATAL: Missing directory '$BO_Project'" && return 1
+[[   -z "$BO_PathLinux"  ]] && echo 'FATAL: Missing $BO_PathLinux'           && return 1
+[[   -z "$BO_PathSystem" ]] && echo 'FATAL: Missing $BO_PathSystem'          && return 1
 
 # Configure environment for Linux
 
@@ -15,7 +17,9 @@ _Script=${_Dir}/activate.bash
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
+
 source ${_Script}
+
 _ExitCode=$?
 [[ ${_ExitCode} -ne 0 ]] && \
   echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
@@ -26,35 +30,17 @@ _ExitCode=$?
 _Dir=$BO_Home/activation/Python
 [[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
 
-_Script=${_Dir}/configure_TMPDIR
-[[ ! -f "${_Script}" ]] && \
-  echo "FATAL: Missing script '${_Script}'" && \
-  return 1
-source ${_Script}
-_ExitCode=$?
-[[ ${_ExitCode} -ne 0 ]] && \
-  echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
-  return ${_ExitCode}
-
 _Script=${_Dir}/configure_output
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
 source ${_Script}
-_ExitCode=$?
-[[ ${_ExitCode} -ne 0 ]] && \
-  echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
-  return ${_ExitCode}
 
 _Script=${_Dir}/configure_pip
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
 source ${_Script}
-_ExitCode=$?
-[[ ${_ExitCode} -ne 0 ]] && \
-  echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
-  return ${_ExitCode}
 
 _Script=${_Dir}/configure_virtualenv
 [[ ! -f "${_Script}" ]] && \
@@ -71,10 +57,15 @@ _Script=${_Dir}/configure_PATH
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
 source ${_Script}
-_ExitCode=$?
-[[ ${_ExitCode} -ne 0 ]] && \
-  echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
-  return ${_ExitCode}
+
+BO_PathProject=$BO_Project/bin/Linux:$BO_Project/PVE/bin
+BO_PathPython=$BO_Home/invocation/Python
+
+PATH=${BO_PathProject}
+PATH=$PATH:${BO_PathPython}
+PATH=$PATH:${BO_PathLinux}
+PATH=$PATH:${BO_PathSystem}
+export PATH
 
 # Return, but do NOT exit, with a success code
 return 0
