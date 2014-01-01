33385157e0351451289b7db3426909c807f6fcf1
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 09:48:08 -0700

    Revisit design of 'activation' scripts

--- activation/Linux/declare.bash
+++ activation/Linux/declare.bash
@@ -4,9 +4,9 @@ echo 'INFO: Declaring all scripting functionality for BriteOnyx...'
 [[ -z "$BO_Home" ]] && echo 'FATAL: Missing $BO_Home' && return 1
 _Dir=$BO_Home/activation/Linux
 [[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
-source ${_Dir}/declare-logging.bash
-source ${_Dir}/declare-BASH.bash
-source ${_Dir}/declare-POSIX.bash
+# source ${_Dir}/declare-logging.bash
+# source ${_Dir}/declare-BASH.bash
+# source ${_Dir}/declare-POSIX.bash
 
 # Return, but do NOT exit, with a success code
 return 0
