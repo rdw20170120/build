9a2fa7bc69cd12f7b9f00e1314e8a4c333a26b00
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 16:39:36 -0700

    Revisit design of 'activation' scripts

--- activation/Python/activate.bash
+++ activation/Python/activate.bash
@@ -6,8 +6,7 @@ echo "TRACE: Executing '$BASH_SOURCE'"
 [[   -z "$BO_Project" ]] && echo 'FATAL: Missing $BO_Project'             && return 1
 [[ ! -d "$BO_Project" ]] && echo "FATAL: Missing directory '$BO_Project'" && return 1
 
-[[ -z "$BO_PathSystem"      ]] && echo 'FATAL: Missing $BO_PathSystem'      && return 1
-[[ -z "$PIP_DOWNLOAD_CACHE" ]] && echo 'FATAL: Missing $PIP_DOWNLOAD_CACHE' && return 1
+[[ -z "$BO_PathSystem" ]] && echo 'FATAL: Missing $BO_PathSystem' && return 1
 
 # Configure environment for Linux
 
@@ -29,6 +28,15 @@ _ExitCode=$?
 # Configure environment for Python on Linux
 [[ -z "$BO_PathLinux" ]] && echo 'FATAL: Missing $BO_PathLinux' && return 1
 
+export BO_PathProject=$BO_Project/bin/Linux:$BO_Project/PVE/bin
+export BO_PathPython=$BO_Home/invocation/Python
+
+PATH=${BO_PathProject}
+PATH=$PATH:${BO_PathPython}
+PATH=$PATH:${BO_PathLinux}
+PATH=$PATH:${BO_PathSystem}
+export PATH
+
 _Dir=$BO_Home/activation/Python
 [[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
 
@@ -38,22 +46,21 @@ _Script=${_Dir}/configure_output
   return 1
 source ${_Script}
 
-createDirectory $PIP_DOWNLOAD_CACHE || return 1
+# Configure PIP_DOWNLOAD_CACHE
+if [[ -z "$PIP_DOWNLOAD_CACHE" ]]; then
+  echo 'WARN: Missing $PIP_DOWNLOAD_CACHE'
+  [[ -z "$PIP_DOWNLOAD_CACHE" ]] && [[ -n "$TMPDIR" ]] && export PIP_DOWNLOAD_CACHE="$TMPDIR/pip"
+  [[ -z "$PIP_DOWNLOAD_CACHE" ]] && echo 'FATAL: Missing $PIP_DOWNLOAD_CACHE' && return 1
+  echo "INFO: Remembering PIP_DOWNLOAD_CACHE='$PIP_DOWNLOAD_CACHE'"
+fi
+[[ ! -d "$PIP_DOWNLOAD_CACHE" ]] && mkdir -p "$PIP_DOWNLOAD_CACHE"
+[[ ! -d "$PIP_DOWNLOAD_CACHE" ]] && echo "FATAL: Missing directory '$PIP_DOWNLOAD_CACHE'" && return 1
 
 _Script=${_Dir}/configure_virtualenv
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
 # source ${_Script}
-
-export BO_PathProject=$BO_Project/bin/Linux:$BO_Project/PVE/bin
-export BO_PathPython=$BO_Home/invocation/Python
-
-PATH=${BO_PathProject}
-PATH=$PATH:${BO_PathPython}
-PATH=$PATH:${BO_PathLinux}
-PATH=$PATH:${BO_PathSystem}
-export PATH
 
 # Return, but do NOT exit, with a success code
 return 0
