eae4d96065e82b56d4bee94bc7f02c1dcc3d50c1
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 10:08:50 -0700

    Revisit design of 'activation' scripts

--- invocation/Linux/helper/declare-Jenkins.bash
+++ invocation/Linux/helper/declare-Jenkins.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
-# Declare BASH functionality for Hudson
+# Declare BASH functionality for the Jenkins continuous integration server
 # TODO: Integrate with latest framework
 
-Hudson_findBuild() {
-  # Return whether found Hudson build for job $2, and save to file $1 if found
+Jenkins_findBuild() {
+  # Return whether found Jenkins build for job $2, and save to file $1 if found
   requireParameters "$FUNCNAME" "$LINENO" 2 "$#"
   requireParameter  "$FUNCNAME" "$LINENO" "$1" 1 'build number file'
-  requireParameter  "$FUNCNAME" "$LINENO" "$2" 2 'Hudson URL'
+  requireParameter  "$FUNCNAME" "$LINENO" "$2" 2 'Jenkins URL'
 
   logInfo "$FUNCNAME" "$LINENO" "Getting build number from '$2'..."
-  _Number=$(Hudson_getBuildNumber "$2")
-  Hudson_saveBuildNumber "$1" "${_Number}"
+  _Number=$(Jenkins_getBuildNumber "$2")
+  Jenkins_saveBuildNumber "$1" "${_Number}"
 }
 
-Hudson_getArtifact () {
-  # Get the Hudson build artifact from URL $1 as file $2
+Jenkins_getArtifact () {
+  # Get the Jenkins build artifact from URL $1 as file $2
   requireParameters "$FUNCNAME" "$LINENO" 2 "$#"
   requireParameter  "$FUNCNAME" "$LINENO" "$1" 1 'artifact target file'
   requireParameter  "$FUNCNAME" "$LINENO" "$2" 2 'artifact URL'
@@ -29,19 +29,19 @@ Hudson_getArtifact () {
   fi
 }
 
-Hudson_getBuildNumber () {
-  # Return the Hudson build number for job $1
+Jenkins_getBuildNumber () {
+  # Return the Jenkins build number for job $1
   # NOTE:  Designed to return result via standard output
   requireParameters "$FUNCNAME" "$LINENO" 1 "$#"
-  requireParameter  "$FUNCNAME" "$LINENO" "$1" 1 'Hudson job'
+  requireParameter  "$FUNCNAME" "$LINENO" "$1" 1 'Jenkins job'
 
   declare -r _Number=$(curl -Ls "$1")
   abortOnFail "$FUNCNAME" "$LINENO" "$?"
   echo "${_Number}"
 }
 
-Hudson_saveBuildNumber() {
-  # Save Hudson build number $2 to file $1
+Jenkins_saveBuildNumber() {
+  # Save Jenkins build number $2 to file $1
   requireParameters "$FUNCNAME" "$LINENO" 2 "$#"
   requireParameter  "$FUNCNAME" "$LINENO" "$1" 1 'target file'
   requireParameter  "$FUNCNAME" "$LINENO" "$2" 2 'build number'
