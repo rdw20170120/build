e7b4fe337a2f842f452467620f0b1a3104171dd3
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 09:46:50 -0700

    Revisit design of 'activation' scripts

--- activation/Linux/declare.bash
+++ activation/Linux/declare.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo 'INFO: Declaring all scripting functionality for BriteOnyx...'
 
-[[ -z "$BO_Home" ]] && echo 'FATAL: Missing $BO_Home' && exit 1
+[[ -z "$BO_Home" ]] && echo 'FATAL: Missing $BO_Home' && return 1
 _Dir=$BO_Home/activation/Linux
-[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && exit 1
+[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
 source ${_Dir}/declare-logging.bash
 source ${_Dir}/declare-BASH.bash
 source ${_Dir}/declare-POSIX.bash
+
+# Return, but do NOT exit, with a success code
+return 0
 
 : <<'DisabledContent'
 # Integrate later, preferably as optional components
@@ -14,7 +17,4 @@ source ${_Dir}/declare-Hudson.bash
 source ${_Dir}/declare-Mercurial.bash
 source ${_Dir}/declare-Subversion.bash
 DisabledContent
-
-# Return, but do NOT exit, with a success code
-return 0
 
