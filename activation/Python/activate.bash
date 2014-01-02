50147069b56fcfa5d06ddb4af62a0e9f978cf69d
Rob Williams <RDW.20091028a.2bq64D@refactory.biz>
2014-01-01 17:02:26 -0700

    Revisit design of 'activation' scripts

--- activation/Python/activate.bash
+++ activation/Python/activate.bash
@@ -28,7 +28,8 @@ _ExitCode=$?
 # Configure environment for Python on Linux
 [[ -z "$BO_PathLinux" ]] && echo 'FATAL: Missing $BO_PathLinux' && return 1
 
-export BO_PathProject=$BO_Project/bin/Linux:$BO_Project/PVE/bin
+_DirPVE=$BO_Project/PVE
+export BO_PathProject=$BO_Project/bin/Linux:${_DirPVE}/bin
 export BO_PathPython=$BO_Home/invocation/Python
 
 PATH=${BO_PathProject}
@@ -36,15 +37,6 @@ PATH=$PATH:${BO_PathPython}
 PATH=$PATH:${BO_PathLinux}
 PATH=$PATH:${BO_PathSystem}
 export PATH
-
-_Dir=$BO_Home/activation/Python
-[[ ! -d "${_Dir}" ]] && echo "FATAL: Missing directory '${_Dir}'" && return 1
-
-_Script=${_Dir}/configure_output
-[[ ! -f "${_Script}" ]] && \
-  echo "FATAL: Missing script '${_Script}'" && \
-  return 1
-source ${_Script}
 
 # Configure PIP_DOWNLOAD_CACHE
 if [[ -z "$PIP_DOWNLOAD_CACHE" ]]; then
@@ -56,11 +48,43 @@ fi
 [[ ! -d "$PIP_DOWNLOAD_CACHE" ]] && mkdir -p "$PIP_DOWNLOAD_CACHE"
 [[ ! -d "$PIP_DOWNLOAD_CACHE" ]] && echo "FATAL: Missing directory '$PIP_DOWNLOAD_CACHE'" && return 1
 
-_Script=${_Dir}/configure_virtualenv
+# Configure Python virtual environment (PVE)
+export PIP_REQUIRE_VIRTUALENV=true
+export PIP_RESPECT_VIRTUALENV=true
+
+_Script=${_DirPVE}/bin/activate
+if [[ ! -f "${_Script}" ]]; then
+  # If the virtual environment does not already exist, create it
+  # TODO: This code assumes that the Python virtual environment package is
+  # already installed, but it may not be.  Eventually we should handle that,
+  # either with a more-specific message or by actually installing it.
+  echo "WARN: Creating Python virtual environment (PVE) in '${_DirPVE}'"
+  echo "WARN: This requires the 'python-virtualenv' package to have been installed"
+  virtualenv "${_DirPVE}"
+  _ExitCode=$?
+  [[ ${_ExitCode} -ne 0 ]] && \
+    echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
+    return ${_ExitCode}
+fi
+
+[[ ! -d "${_DirPVE}" ]] && echo "FATAL: Missing directory '${_DirPVE}'" && return 1
 [[ ! -f "${_Script}" ]] && \
   echo "FATAL: Missing script '${_Script}'" && \
   return 1
-# source ${_Script}
+
+source ${_Script}
+
+_ExitCode=$?
+[[ ${_ExitCode} -ne 0 ]] && \
+  echo "FATAL: Exit code ${_ExitCode} at '$BASH_SOURCE':$LINENO" && \
+  return ${_ExitCode}
+
+[[ -z "$VIRTUAL_ENV" ]] && echo 'FATAL: Missing $VIRTUAL_ENV' && return 1
+export PYTHONHOME=$VIRTUAL_ENV
+[[ -z "$PYTHONHOME"  ]] && echo 'FATAL: Missing $PYTHONHOME'  && return 1
+
+echo "INFO: Activated Python virtual environment (PVE) in '${_DirPVE}'"
+echo "INFO: Found '$(python --version 2>&1)' at '$(which python)'"
 
 # Return, but do NOT exit, with a success code
 return 0
