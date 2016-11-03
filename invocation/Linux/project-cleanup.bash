a998d27e39cb0f0d06c20edc4fe269ba27bf35da
Rob Williams <rdw.bitbucket.robwilliams@refactory.biz>
2016-11-03 14:43:21 -0600

    Tweak project scripts

--- invocation/Linux/project-cleanup.bash
+++ invocation/Linux/project-cleanup.bash
@@ -3,8 +3,8 @@
 # NOTE: Assumes that this project has been activated using the BriteOnyx framework.
 ###################################################################################################
 
-variableRequire 'BO_Project'
-directoryRequire "$BO_Project"
+variableRequire   BO_Project
+directoryRequire $BO_Project
 
 ###################################################################################################
 logInfo 'Cleaning up in project'
--- invocation/Linux/project-fix-permissions.bash
+++ invocation/Linux/project-fix-permissions.bash
@@ -2,18 +2,17 @@
 [[ -n "$BO_Trace" ]] && echo "TRACE: Executing '$BASH_SOURCE'"
 # NOTE: Assumes that this project has been activated using the BriteOnyx framework.
 # NOTE: Apple Mac OSX BASH find does not support -perm with "/" syntax
-# TODO: FIX: Make this work on OSX as well as Linux
+# TODO: SOMEDAY FIX: Make this work on OSX (unrecognized syntax)
+# TODO: SOMEDAY Refactor individual commands into BASH functions
 ###################################################################################################
 
-variableRequire 'BO_Project'
-directoryRequire "$BO_Project"
+variableRequire   BO_Project
+directoryRequire $BO_Project
 
 fixPermissions () {
   logInfo "Fixing POSIX permissions within path '$1'"
   parametersRequire 1 "$#"
   valueRequire "$1" 'path'
-
-  # TODO: SOMEDAY FIX does not work on OSX due to unrecognized syntax
 
   # Make all directories accessible by the owner
   find "$1" -type d \! -perm -u+rw -exec ls -al     '{}' \;
@@ -52,7 +51,7 @@ fixPermissions () {
 ###################################################################################################
 logInfo "Fixing POSIX permissions in project '$BO_Project'"
 
-fixPermissions $BO_Project
+fixPermissions $1
 
 ###################################################################################################
 : <<'DisabledContent'
