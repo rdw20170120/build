9e038241c7b255b54c8e0ac51423f2fb0e83326c
Rob Williams <rdw.bitbucket.robwilliams@refactory.biz>
2017-02-24 01:50:04 +0000

    Fix bug in maybeActivate.src

--- src/piece/bootstrap/sample_project/BriteOnyx/activateFromProjectScript.src/000-skip.bash
+++ src/piece/bootstrap/sample_project/BriteOnyx/activateFromProjectScript.src/000-skip.bash
@@ -2,5 +2,5 @@
 # Skip if BriteOnyx is already activated
 
 Msg='$BO_Home is defined, assuming BriteOnyx already activated'
-[[ -n "$BO_Home" ]] && logDebug "$Msg" && return 63
+[[ -n "$BO_Home" ]] && logDebug "$Msg" && return 0
 
