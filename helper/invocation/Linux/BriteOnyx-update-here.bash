842f02a2d33e9c0f0eb76faaf1800bebb7decc88
Rob Williams <rdw.bitbucket.robwilliams@refactory.biz>
2017-03-22 18:04:38 +0000

    Fix BriteOnyx-update-here.bash to force copy of files

--- helper/invocation/Linux/BriteOnyx-update-here.bash
+++ helper/invocation/Linux/BriteOnyx-update-here.bash
@@ -5,14 +5,10 @@
 # of a BriteOnyx-enabled project.  BriteOnyx content is taken from $BO_Home, if configured.  Use
 # first argument as $BO_Home, if provided.  Project must be re-activated to apply the new content.
 ####################################################################################################
-# NOTE: Uncomment the following two lines for debugging
-# set -o verbose
-# set -o xtrace
 
-####################################################################################################
 main () {
   # Optional parameter 1 is source directory to be used as BO_Home
-  [[ -n "$1" ]] && BO_Home=$1
+  [[ -n "$1" ]] && local -r BO_Home=$1
   [[ -z "$BO_Home" ]] && echo 'FATAL: source directory was not provided' && exit 1
 
   local -r DirHere=$PWD
@@ -21,24 +17,30 @@ main () {
 
   echo "Updating/installing BriteOnyx content here in directory '$DirHere', from directory '$BO_Home'"
   [[ -d "$DirTgt" ]] && rm -r $DirTgt
-  cp $BO_Home/sample_project/activate.src $DirHere/activate.src
+  cp -fp $BO_Home/sample_project/activate.src $DirHere/activate.src
 
   # Handle new content
-  mkdir                         $DirTgt
-  cp $DirSrc/declare.src        $DirTgt
-  cp $DirSrc/env.src            $DirTgt
-  cp $DirSrc/maybeActivate.src  $DirTgt
-  mkdir                                           $DirTgt/starter
-  cp $DirSrc/starter/project.hgignore             $DirTgt/starter
-  cp $DirSrc/starter/project-fix-permissions.bash $DirTgt/starter
-  cp $DirSrc/starter/project-env.src              $DirTgt/starter
-  cp $DirSrc/starter/user-BriteOnyx.src           $DirTgt/starter
+  mkdir                             $DirTgt
+  cp -fp $DirSrc/declare.src        $DirTgt
+  cp -fp $DirSrc/env.src            $DirTgt
+  cp -fp $DirSrc/maybeActivate.src  $DirTgt
+  mkdir                                               $DirTgt/starter
+  cp -fp $DirSrc/starter/project.hgignore             $DirTgt/starter
+  cp -fp $DirSrc/starter/project-fix-permissions.bash $DirTgt/starter
+  cp -fp $DirSrc/starter/project-env.src              $DirTgt/starter
+  cp -fp $DirSrc/starter/user-BriteOnyx.src           $DirTgt/starter
 }
+
+####################################################################################################
+# NOTE: Uncomment the following two lines for debugging
+# set -o verbose
+# set -o xtrace
 
 main $@
 
 ####################################################################################################
 : <<'DisabledContent'
   # Handle old content
-  cp -R $DirSrc  $DirTgt
+  cp -R $DirSrc $DirTgt
 DisabledContent
+
