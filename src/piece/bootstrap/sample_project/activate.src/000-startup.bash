5cec8f51403589e174d7db6201873aad4673cd57
Rob Williams <rdw.bitbucket.robwilliams@refactory.biz>
2017-07-25 21:05:47 +0000

    Add logging to file BO.log

--- src/piece/bootstrap/sample_project/activate.src/000-startup.bash
+++ src/piece/bootstrap/sample_project/activate.src/000-startup.bash
@@ -1,5 +1,11 @@
 # Startup
 
+if [[ -n "$BO_Home" ]] ; then
+  echo "Activating..." >$BO_Home/BO.log
+else
+  echo "Activating..." >$PWD/BO.log
+fi
+
 echo "INFO:  Activating the BriteOnyx framework for this project..."
 echo "WARN:  This script MUST be executed as 'source activate.src', WAS IT?"
 env | sort >./BO-env-incoming.out
