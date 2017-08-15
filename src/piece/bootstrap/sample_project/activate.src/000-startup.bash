44f186e2697b1130dcdca70bfd96e3f6b694f157
Rob Williams <rdw.bitbucket.robwilliams@refactory.biz>
2017-08-15 16:45:40 +0000

    Enhance with random TMPDIR

--- src/piece/bootstrap/sample_project/activate.src/000-startup.bash
+++ src/piece/bootstrap/sample_project/activate.src/000-startup.bash
@@ -1,8 +1,8 @@
-# Startup
-
+# Create random TMPDIR
 Dir=$(mktemp --tmpdir -d BO-XXXXXXXX)
 [[ -d "$Dir" ]] && export TMPDIR=$Dir
 
+# Remember BriteOnyx logging file
 BO_FileLog=BO.log
 if [[ -n "$TMPDIR" ]] ; then
   export BO_FileLog=$TMPDIR/$BO_FileLog
@@ -11,9 +11,16 @@ elif [[ -n "$BO_Project" ]] ; then
 else
   export BO_FileLog=$PWD/$BO_FileLog
 fi
-echo "Activating..." >$BO_FileLog
-
+echo "INFO:  Activating..." >$BO_FileLog
 echo "INFO:  Activating the BriteOnyx framework for this project..."
 echo "WARN:  This script MUST be executed as 'source activate.src', WAS IT?"
-env | sort >./BO-env-incoming.out
+
+# Capture incoming BASH environment
+if [[ -n "$TMPDIR" ]] ; then
+  env | sort >$TMPDIR/BO-env-incoming.out
+elif [[ -n "$BO_Project" ]] ; then
+  env | sort >$BO_Project/BO-env-incoming.out
+else
+  env | sort >$PWD/BO-env-incoming.out
+fi
 
--- src/piece/bootstrap/sample_project/activate.src/027-shutdown.bash
+++ src/piece/bootstrap/sample_project/activate.src/027-shutdown.bash
@@ -1,5 +1,13 @@
 # Shutdown
 
 logInfo "Project '$BO_ProjectName' in directory '$BO_Project' is now activated, done."
-env | sort >./BO-env-outgoing.out
+
+# Capture outgoing BASH environment
+if [[ -n "$TMPDIR" ]] ; then
+  env | sort >$TMPDIR/BO-env-outgoing.out
+elif [[ -n "$BO_Project" ]] ; then
+  env | sort >$BO_Project/BO-env-outgoing.out
+else
+  env | sort >$PWD/BO-env-outgoing.out
+fi
 
